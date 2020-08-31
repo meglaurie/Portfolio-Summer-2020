c6e8c76471c9810c7edc151af55f9b1ee4eb0f81
meglaurie <megan.laurie@edu.sait.ca>
2020-08-31 14:02:41 -0600

    Image resizing for archive page

--- my-react-website/src/pages/ProjectContent.js
+++ my-react-website/src/pages/ProjectContent.js
@@ -2,13 +2,13 @@ const projects = [
     {
         name: 'Project 1',
         title: 'Pokemon API App',
-        image: require('../imgs/HelloThere.png'),
+        image: require('../imgs/pokemon.png'),
         alt: 'alt-text',
         url: '',
         content: [
             `Description`,
         ]
-    },    
+    },
 ];
 
 export default projects;
