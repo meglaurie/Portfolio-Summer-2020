7a29691b5408c1199440d82335ebe17c3fdeb481
meglaurie <megan.laurie@edu.sait.ca>
2020-09-23 11:41:37 -0600

    Design changes

--- my-react-website/src/components/DesignProjectList.js
+++ my-react-website/src/components/DesignProjectList.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { faCodepen } from '@fortawesome/free-brands-svg-icons';
 import { faDribbble } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
 const DesignProjectList = ({ designProjects }) => (
+
   <>
   {designProjects.map((designProjects, key) => (
     <section key={key} to={`/designProjects/${designProjects.name}`}>
@@ -12,7 +12,7 @@ const DesignProjectList = ({ designProjects }) => (
         <img className="projectImg" src={designProjects.image} alt={designProjects.alt}/>
         <h3 className="project-title">{designProjects.title}</h3>
         <p className="project-desc">{designProjects.content[0]}</p>
-        <a className="fa-icon project-icon" target="_blank" rel="noopener noreferrer" aria-label="Codepen external link" href={designProjects.codepen}><FontAwesomeIcon icon={faCodepen} /></a>
+        <a className="fa-icon project-icon" target="_blank" rel="noopener noreferrer" aria-label="Codepen external link" href={designProjects.codepen }><FontAwesomeIcon icon={faCodepen} /></a>
         <a className="fa-icon project-icon" target="_blank" rel="noopener noreferrer" aria-label="Dribble external link" href={designProjects.dribbble}><FontAwesomeIcon icon={faDribbble} /></a>
       </div>
     </section>
