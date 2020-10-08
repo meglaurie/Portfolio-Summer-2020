e98b9718d313105d7af4c6a157462c681ada4ef5
meglaurie <megan.laurie@edu.sait.ca>
2020-10-08 16:04:04 -0600

    added github icon

--- my-react-website/src/components/DesignProjectList.js
+++ my-react-website/src/components/DesignProjectList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { faCodepen } from '@fortawesome/free-brands-svg-icons';
 import { faDribbble } from '@fortawesome/free-brands-svg-icons';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DesignProjectList = ({ designProjects }) => (
@@ -14,6 +15,7 @@ const DesignProjectList = ({ designProjects }) => (
         <p className="project-desc">{designProjects.content[0]}</p>
         <a className={designProjects.codepen == '' ? "display-none" : "fa-icon project-icon" } target="_blank" rel="noopener noreferrer" aria-label="Codepen external link" href={designProjects.codepen}><FontAwesomeIcon icon={faCodepen} /></a>
         <a className={designProjects.dribbble == '' ? "display-none" : "fa-icon project-icon" } target="_blank" rel="noopener noreferrer" aria-label="Dribble external link" href={designProjects.dribbble}><FontAwesomeIcon icon={faDribbble} /></a>
+        <a className={designProjects.github == '' ? "display-none" : "fa-icon project-icon" } target="_blank" rel="noopener noreferrer" aria-label="Codepen external link" href={designProjects.github}><FontAwesomeIcon icon={faGithubAlt} /></a>
       </div>
     </section>
 
--- my-react-website/src/pages/DesignProjectContent.js
+++ my-react-website/src/pages/DesignProjectContent.js
@@ -6,6 +6,7 @@ const designProjects = [
         alt: 'Icon set with 12 different kinds of bubble tea.',
         codepen: '',
         dribbble: 'https://dribbble.com/shots/13942532-BubbleTea-Icon-Set',
+        github: '',
         content: [
             `PNG and SVG icon set made using Sketch. Available for free download on Github.`,
         ]
@@ -16,6 +17,7 @@ const designProjects = [
         alt: 'Hello There neon sign from Batman returns',
         codepen: 'https://codepen.io/mlaur568/pen/LYNGYjB',
         dribbble: 'https://dribbble.com/shots/14017708-Hello-There-Batman-Returns',
+        github: 'https://github.com/meglaurie/CSS-Excercises/tree/master/HelloThere',
         content: [
             `Neon sign inspired by Batman Returns. Made with HTML and CSS3`,
         ]
@@ -26,6 +28,7 @@ const designProjects = [
         alt: 'Pink background with Head of Luna from Sailor Moon in the center',
         codepen: 'https://codepen.io/mlaur568/pen/jOqPoyg',
         dribbble: '',
+        github: 'https://github.com/meglaurie/CSS-Excercises/tree/master/Luna',
         content: [
             `Luna made with HTML and CSS`,
         ]
@@ -36,6 +39,7 @@ const designProjects = [
         alt: 'Vector art work space; desk, chair, cat, lamp, and other trinkets',
         codepen: '',
         dribbble: 'https://dribbble.com/shots/13956797-Office-Desk-Vector-Art',
+        github: '',
         content: [
             `Vector graphic art created using sketch.`,
         ]
